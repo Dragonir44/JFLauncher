f23ad6cb8ecfbec315e826fd91d3ee5ecc6ef96d
fthaumur <flavien.thaumur@nextdecision.fr>
2023-11-17 15:43:50 +0100

    Add login functionality and error handling

---
+++ src/public/scripts/script.js
@@ -0,0 +1,31 @@
+// Importation des modules
+const ipc = window.ipc
+const localStorage = window.localStorage
+const store = new window.Store()
+
+// Variables globales
+let playbtn = document.getElementById("play");
+
+// Lors d'un click sur le bouton play, non désactivé
+playbtn.addEventListener("click", () => {
+  playbtn.disabled = true;
+  ipc.send("login", {})
+});
+
+if (store.get("userDetails")) {
+    playbtn.disabled = true;
+    ipc.send("loginToken", JSON.parse(store.get("userDetails")));
+}
+
+// Récéption de l'event erreur
+ipc.on("err", (event, errorMessage) => {
+  localStorage.clear();
+  iziToast.error({
+    id: "error",
+    title: "Erreur",
+    message: errorMessage,
+    position: "bottomRight",
+    transitionIn: "fadeInDown",
+  });
+  playbtn.disabled = false;
+});
