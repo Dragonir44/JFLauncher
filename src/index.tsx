7b39621d78e7626ed3e77544116ebe52ac3aaef0
Dragonir44 <dragonir44@gmail.com>
2023-11-21 19:31:24 +0100

    Add UpdateLauncher component to default route

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
+import UpdateLauncher from "screens/updateLauncher";
 import Auth from "screens/auth";
 import Launcher from "screens/launcher";
 
@@ -30,7 +31,8 @@ root.render(
         <div id="titlebarRegion" className="titlebarRegion"></div>
         <HashRouter>
             <Routes>
-                <Route path="/" element={<Auth />} />
+                <Route path="/" element={<UpdateLauncher/>} />
+                <Route path="/auth" element={<Auth />} />
                 <Route path="/launcher" element={<Launcher />} />
             </Routes>
         </HashRouter>
