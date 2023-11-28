51b5bc6c17af29f07d91808ce56f9af7d24d3385
Dragonir44 <dragonir44@gmail.com>
2023-11-28 14:47:35 +0100

    Refactor auth screen and add account list

--- src/screens/auth.tsx
+++ src/screens/auth.tsx
@@ -3,21 +3,32 @@ import { withRouter } from "utils/withRouter";
 import { NavigateFunction } from "react-router-dom";
 import Footer from "screens/footer";
 
-import 'css/style.css'
+import 'scss/auth.scss'
 
 type Props = {
     navigate?: NavigateFunction;
 }
 
-class Auth extends Component<Props> {
+type State = {
+    accounts: any;
+}
+
+class Auth extends Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            accounts: [],
+        }
+    }
 
     async componentDidMount() {
         try {
             const userDetails = await window.store.get("userDetails")
-            if (userDetails) {
-                const playbtn = document.getElementById("play") as HTMLButtonElement
-                playbtn.disabled = true;
-                window.ipc.send("loginToken", JSON.parse(userDetails))
+            const accounts = JSON.parse(await window.store.get("token"))
+            if (accounts && this.state.accounts.length === 0) {
+                this.setState({ accounts: [{userDetails: userDetails, account: accounts}] });
             }
             window.ipc.receive("auth-success", () => {
                 this.props.navigate!(`/launcher`);
@@ -36,11 +47,49 @@ class Auth extends Component<Props> {
     }
 
     render() {
+        const { accounts } = this.state
+        let countAccounts = 0
         return (
             <>
                 <div className="login-page">
-                    <div className="form">
-                        <button id="play" onClick={this.handleConnect}>Connexion</button>
+
+                    <div className="accountList">
+                        {accounts.map((data: any) => {
+                            if (data.account.profile.name) {
+                                data.id = countAccounts++
+                                return (
+                                    <div className="account" key={data.account.profile.id}>
+                                        <img src={`https://mc-heads.net/avatar/${data.account.profile.name}`} className="skin" />
+                                        <div className="accountName">{data.account.profile.name}</div>
+
+                                        <div className="accountButtons">
+                                            <button className="accountButton play" onClick={(e) => {
+                                                e.currentTarget.disabled = true;
+                                                window.ipc.send("loginToken", JSON.parse(data.userDetails))
+                                            }}>
+                                            <img src="assets/play.svg" className="accountButtonIcon" />
+                                            </button>
+                                            {/* <button className="accountButton delete" onClick={async () => {
+                                                const accounts = JSON.parse(await window.store.get("token") || "{}")
+                                                console.log(accounts[data.id])
+                                                // delete accounts[data.id-1]
+                                                // window.store.set("token", JSON.stringify(accounts))
+                                                // this.setState({ accounts: Object.values(accounts) });
+                                            }}>
+                                                <img src="assets/delete.svg" className="accountButtonIcon" />
+                                            </button> */}
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        })}
+                        {
+                            accounts.length < 2 && 
+                            <button className="addAccount" onClick={this.handleConnect}>
+                                <img src="assets/addAccount.svg" className="addAccountIcon" />
+                                <div className="addAccountText">Ajouter un compte</div>
+                            </button>
+                        }
                     </div>
                 </div>
                 <Footer />
